3190e0d1cef84f030394363e3dd16596f4ba6298
Nayara <nayaraazevwdo@gmail.com>
2022-07-19 16:34:11 -0300

    turma-Barbosa-D

---
+++ exercicio1/script.js
@@ -0,0 +1,20 @@
+// INÍCIO DO EXERCÍCIO
+
+// const nome 
+// const idade 
+// console.log(typeof nome)
+// console.log(typeof idade)
+// Erro:Inicializador ausente na declaração const,pois não foi atríbuido nenhum valor as variaveis 
+
+ // Atualização 
+ const nome = "Bruna"
+ const idade = 24
+ prompt('Qual é o seu nome?')
+ prompt("Qual é a sua idade?")
+ console.log(typeof nome)
+ console.log(typeof idade)
+ // Pude observar que não deu nenhum erro ,apareceu as duas caixinhas de pergunta ,que é uma string e um number
+  console.log("Hi", nome , "você tem", idade ,"anos" )
+ 
+ 
+
---
+++ exercicio3/script.js
@@ -0,0 +1,15 @@
+// VALORES
+
+ let a = 10
+ let b = 25
+ let c = 0
+
+ // TROCANDO OS VALORES
+
+ c = a
+ a = b 
+ b = c
+
+console.log("a" , a)
+console.log("b" , b)
+console.log("c" , c)
